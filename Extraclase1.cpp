c2b351a205b0dcf80da3218571831e8bd54d2379
Meibel Ceciliano <cecilianomeibel@gmail.com>
2022-08-23 19:03:26 -0600

    Creacion de clase PagedArray

---
+++ Extraclase1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class PagedArray{
+
+    public:
+    void prueba();
+
+};
+
+void PagedArray::prueba(){
+    cout<<"Hola mundo"<<endl;
+    
+}
+
+
+
+int main()
+{
+    PagedArray objeto; 
+    objeto.prueba();
+    
+	return 0;
+
+}
